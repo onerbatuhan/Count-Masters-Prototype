8167a9de0a647aa0f947f9146ef94cdea9c47220
Batuhan Öner <batuhanacademy@gmail.com>
2023-07-08 20:58:28 +0300

    PlatformRoad update and Fallcontroller ımport

--- Assets/Scripts/Movement/FallController.cs
+++ Assets/Scripts/Movement/FallController.cs
@@ -7,24 +7,48 @@ namespace Movement
 {
     public class FallController : MonoBehaviour
     {
+        private enum FallState
+        {
+            Preparation,
+            ExitPreparation
+        }
+        [SerializeField] private FallState fallState;
         private void OnTriggerEnter(Collider other)
         {
             ObjectTypes objectTypes;
-            if (other.gameObject.TryGetComponent(out objectTypes) && objectTypes.objectType == ObjectTypes.Type.Character)
+            if (!other.gameObject.TryGetComponent(out objectTypes) || objectTypes.objectType != ObjectTypes.Type.Character) return;
+            switch (fallState)
             {
-                PrepareForFall(other.gameObject);
+                case FallState.Preparation:
+                    PrepareForFall(other.gameObject);
+                    break;
+                case FallState.ExitPreparation:
+                    ExitFallPreparation(other.gameObject);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
         private void PrepareForFall(GameObject playerObject)
         {
-            Rigidbody rigidBody = playerObject.GetComponent<Rigidbody>();
-            CapsuleCollider capsuleCollider = playerObject.GetComponent<CapsuleCollider>();
-            NavMeshAgent navMeshAgent = playerObject.GetComponent<NavMeshAgent>();
-            navMeshAgent.enabled = false;
-            rigidBody.useGravity = true;
-            capsuleCollider.isTrigger = false;
-            
+            SetCharacterProperties(playerObject, useGravity: true, isTrigger: false);
+        }
+
+        private void ExitFallPreparation(GameObject playerObject)
+        {
+            SetCharacterProperties(playerObject, useGravity: false, isTrigger: true);
+        }
+
+        private void SetCharacterProperties(GameObject playerObject, bool useGravity, bool isTrigger)
+        {
+            var rigidBody = playerObject.GetComponent<Rigidbody>();
+            var capsuleCollider = playerObject.GetComponent<CapsuleCollider>();
+            var navMeshAgent = playerObject.GetComponent<NavMeshAgent>();
+
+            navMeshAgent.enabled = !useGravity;
+            rigidBody.useGravity = useGravity;
+            capsuleCollider.isTrigger = isTrigger;
         }
     }
 }
