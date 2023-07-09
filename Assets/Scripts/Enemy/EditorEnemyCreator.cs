0406a991d2e13c860d35d1cfe3ab439e08889f7e
Batuhan Öner <batuhanacademy@gmail.com>
2023-07-09 20:54:10 +0300

    Editor enemy creator start working and enemy-attack system

--- Assets/Scripts/Enemy/EditorEnemyCreator.cs
+++ Assets/Scripts/Enemy/EditorEnemyCreator.cs
@@ -1,9 +1,71 @@
+
+using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+#endif
 
 namespace Enemy
 {
     public class EditorEnemyCreator : MonoBehaviour
     {
-      
+        [SerializeField] private GameObject referenceObject;
+        [SerializeField] private int enemyCount;
+        private float smallCircleRadius = .3f;
+        private float smallCircleSpacing = 1;
+        private GameObject _enemyGroupObject;
+        
+
+        private void OnDrawGizmos()
+        {
+#if UNITY_EDITOR
+            Handles.color = Color.red;
+            Handles.DrawWireDisc(transform.position, transform.up, 2f);
+
+            DrawSmallCircles();
+#endif
+        }
+
+#if UNITY_EDITOR
+        [CustomEditor(typeof(EditorEnemyCreator))]
+        
+        public class EditorObjectSpawnerEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                DrawDefaultInspector();
+
+                EditorEnemyCreator spawner = (EditorEnemyCreator)target;
+                if (GUILayout.Button("Create Enemy Group"))
+                {
+                    spawner.CreateEnemyGroup();
+                }
+            }
+        }
+#endif
+
+        private void DrawSmallCircles()
+        {
+#if UNITY_EDITOR
+            Handles.color = Color.red;
+            float angleStep = 360f / enemyCount;
+            float totalSpacing = smallCircleSpacing * (enemyCount - 1);
+            float adjustedRadius = smallCircleRadius + smallCircleSpacing;
+
+            Quaternion rotation = Quaternion.AngleAxis(angleStep, transform.up);
+            Vector3 direction = transform.forward * adjustedRadius;
+
+            for (int i = 0; i < enemyCount; i++)
+            {
+                Handles.DrawWireDisc(transform.position + direction, transform.up, smallCircleRadius);
+                direction = rotation * direction;
+            }
+#endif
+        }
+
+        private void CreateEnemyGroup()
+        {
+            _enemyGroupObject = Instantiate(referenceObject);
+            _enemyGroupObject.transform.position = transform.position;
+        }
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,5 +1,6 @@
 using System;
 using Attack;
+using UnityEditor;
 using UnityEngine;
 
 namespace Enemy
@@ -7,6 +8,7 @@ namespace Enemy
     public class EnemyController : MonoBehaviour
     {
         private AttackController _attackController;
+        public int enemyCount;
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.TryGetComponent(out _attackController))
@@ -14,5 +16,25 @@ namespace Enemy
                 Debug.Log("player takım trigger");
             }
         }
+        
+        
+        
+        
+        
+        
+        
+        private void OnDrawGizmos()
+        {
+#if UNITY_EDITOR
+            Handles.color = Color.green;
+            Handles.DrawSolidDisc(transform.position, transform.up, 2f);
+#endif
+        }
     }
+    
+    
+    
+    
+    
+    
 }
